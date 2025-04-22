7eb29861cd98f4521ccdada618542548928e2008
Preetish Bakliwal <pbakliwal@adobe.com>
2025-04-22 14:20:49 +0530

    cf fix

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -3,7 +3,7 @@
 const AEM_HOST = checkDomain()
 
 function checkDomain(){
-  if (window.location.hostname.includes("hlx.page") || window.location.hostname.includes("localhost")){
+  if (window.location.hostname.includes("aem.page") || window.location.hostname.includes("localhost") || window.location.hostname.includes("aem.live")){
     return "https://publish-p131639-e1282833.adobeaemcloud.com/"    
   }else{
     return window.location.origin 
